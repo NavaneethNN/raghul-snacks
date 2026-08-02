5cedffa35bc1f1e33e17d396ea8ed82042df78f2
Nigalya78 <nigalya1414@gmail.com>
2026-08-02 18:31:28 +0530

    recent changes

---
+++ check.js
@@ -0,0 +1,12 @@
+﻿const b = document.querySelector('.hero-banner');
+const h = document.querySelector('.hero');
+if (b && h) {
+  const bs = getComputedStyle(b);
+  const hs = getComputedStyle(h);
+  const br = b.getBoundingClientRect();
+  const hr = h.getBoundingClientRect();
+  console.log('BANNER:', JSON.stringify({w:br.width,h:br.height,top:br.top,bottom:br.bottom,pos:bs.position,overflow:bs.overflow,display:bs.display,zIndex:bs.zIndex}));
+  console.log('HERO:', JSON.stringify({w:hr.width,h:hr.height,top:hr.top,bottom:hr.bottom,overflow:hs.overflow,paddingBottom:hs.paddingBottom}));
+} else {
+  console.log('NOT_FOUND', !!b, !!h);
+}
